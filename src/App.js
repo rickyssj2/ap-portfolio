8e5a2ca6f81fb5d20ab461aa220a97a24e11584c
Aryan Pandey <aryanpandey048@gmail.com>
2025-03-02 09:11:17 -0800

    Added components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Sidebar from './components/sidebar/Sidebar';
+import Home from './components/home/Home';
+import About from './components/about/About';
+import Resume from './components/resume/Resume';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <>
+            <Sidebar />
+            <main>
+                <Home />
+                <About />
+                <Resume />
+            </main>
+        </>
+    );
+};
 
 export default App;
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import './sidebar.css';
+import Logo from '../../assests/logo.svg';
+
+const Sidebar = () => {
+    return (
+        <aside className="aside">
+            <a href="#home" className='"nav__logo'>
+                <img src={Logo} alt="logo" />
+            </a>
+            <nav className="nav">
+                <div className="nav__menu">
+                    <ul className="nav__list">
+                        <li className="nav__item">
+                            <a href="#home" className="nav__link">
+                                <i className="icon-home"></i>
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#about" className="nav__link">
+                                <i className="icon-user-following"></i>
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#services" className="nav__link">
+                                <i className="icon-briefcase"></i>
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#resume" className="nav__link">
+                                <i className="icon-graduation"></i>
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#portfolio" className="nav__link">
+                                <i className="icon-layers"></i>
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#blog" className="nav__link">
+                                <i className="icon-note"></i>
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#contact" className="nav__link">
+                                <i className="icon-bubble"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <div className="nav__footer">
+                <span className="copyright">
+                    &copy; {new Date().getFullYear()} -{' '}
+                    {new Date().getFullYear() + 1}.
+                </span>
+            </div>
+        </aside>
+    );
+};
+
+export default Sidebar;
